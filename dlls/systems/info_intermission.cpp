52b743fab8927fc864bce0524ab3cfd29da36190
urgorri <106026141+urgorri@users.noreply.github.com>
2026-08-13 20:35:57 +0000

    Migrate miscellaneous classes out of player.cpp
    - Moved `dlls/player.cpp` to `dlls/core/player.cpp` and retained `CBasePlayer`
    - Extracted `CSprayCan` and `CBloodSplat` to `dlls/weapons/weapon_spraycan.cpp` (with header)
    - Extracted `CDeadHEV` to `dlls/monsters/monster_deadhev.cpp`
    - Extracted `CStripWeapons` to `dlls/gameplay/stripweapons.cpp`
    - Extracted `CRevertSaved` to `dlls/systems/revertsaved.cpp`
    - Extracted `CInfoIntermission` to `dlls/systems/info_intermission.cpp`
    - Updated Makefile and vs2019/hldll.vcxproj files to reflect file moves and inclusions

---
+++ dlls/systems/info_intermission.cpp
@@ -0,0 +1,39 @@
+#include "core/extdll.h"
+#include "core/util.h"
+#include "core/cbase.h"
+
+//=========================================================
+// Multiplayer intermission spots.
+//=========================================================
+class CInfoIntermission:public CPointEntity
+{
+	void Spawn( void );
+	void Think( void );
+};
+
+void CInfoIntermission::Spawn( void )
+{
+	UTIL_SetOrigin( pev, pev->origin );
+	pev->solid = SOLID_NOT;
+	pev->effects = EF_NODRAW;
+	pev->v_angle = g_vecZero;
+
+	pev->nextthink = gpGlobals->time + 2;// let targets spawn!
+
+}
+
+void CInfoIntermission::Think ( void )
+{
+	edict_t *pTarget;
+
+	// find my target
+	pTarget = FIND_ENTITY_BY_TARGETNAME( NULL, STRING(pev->target) );
+
+	if ( !FNullEnt(pTarget) )
+	{
+		pev->v_angle = UTIL_VecToAngles( (pTarget->v.origin - pev->origin).Normalize() );
+		pev->v_angle.x = -pev->v_angle.x;
+	}
+}
+
+LINK_ENTITY_TO_CLASS( info_intermission, CInfoIntermission );
