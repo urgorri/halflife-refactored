0167b502399c893bc412adf7d47df73f3f9ab9b6
Gastón Urgorri <urgorrigaston@gmail.com>
2026-08-13 23:43:39 -0300

    Refactor turret systems into separate files (#10)
    * Refactor turret code by splitting dlls/turret.cpp
    
    - Split `dlls/turret.cpp` into multiple files within `dlls/systems/` directory.
    - Created `dlls/systems/turret_base.cpp` for `CBaseTurret`.
    - Created `dlls/systems/turret.cpp` for `CTurret`.
    - Created `dlls/systems/miniturret.cpp` for `CMiniTurret`.
    - Created `dlls/systems/sentry.cpp` for `CSentry`.
    - Updated `dlls/Makefile` to include the new compilation targets and directory creation.
    - Updated `projects/vs2019/hldll.vcxproj` and `projects/vs2019/hldll.vcxproj.filters` with the new file structure.
    
    * Refactor turret code by splitting dlls/turret.cpp
    
    - Split dlls/turret.cpp into multiple files within dlls/systems/ directory.
    - Created dlls/systems/turret_base.cpp for CBaseTurret.
    - Created dlls/systems/turret.cpp for CTurret.
    - Created dlls/systems/miniturret.cpp for CMiniTurret.
    - Created dlls/systems/sentry.cpp for CSentry.
    - Updated dlls/Makefile to include the new compilation targets and directory creation.
    - Updated projects/vs2019/hldll.vcxproj and projects/vs2019/hldll.vcxproj.filters with the new file structure.
    - Updated linux/Makefile.hldll to include the new files in the compilation process.

---
+++ dlls/systems/miniturret.cpp
@@ -0,0 +1,73 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+
+#include "turret.h"
+#include "monsters.h"
+#include "effects.h"
+#include "weapons.h"
+#include "explode.h"
+
+class CMiniTurret : public CBaseTurret
+{
+public:
+	void Spawn( );
+	void Precache(void);
+	// other functions
+	void Shoot(Vector &vecSrc, Vector &vecDirToEnemy);
+};
+
+LINK_ENTITY_TO_CLASS( monster_miniturret, CMiniTurret );
+
+void CMiniTurret::Spawn()
+{
+	Precache( );
+	SET_MODEL(ENT(pev), "models/miniturret.mdl");
+	pev->health			= gSkillData.miniturretHealth;
+	m_HackedGunPos		= Vector( 0, 0, 12.75 );
+	m_flMaxSpin = 0;
+	pev->view_ofs.z = 12.75;
+
+	CBaseTurret::Spawn( );
+	m_iRetractHeight = 16;
+	m_iDeployHeight = 32;
+	m_iMinPitch	= -15;
+	UTIL_SetSize(pev, Vector(-16, -16, -m_iRetractHeight), Vector(16, 16, m_iRetractHeight));
+
+	SetThink(&CMiniTurret::Initialize);
+	pev->nextthink = gpGlobals->time + 0.3;
+}
+
+
+void CMiniTurret::Precache()
+{
+	CBaseTurret::Precache( );
+	PRECACHE_MODEL ("models/miniturret.mdl");
+	PRECACHE_SOUND("weapons/hks1.wav");
+	PRECACHE_SOUND("weapons/hks2.wav");
+	PRECACHE_SOUND("weapons/hks3.wav");
+}
+
+void CMiniTurret::Shoot(Vector &vecSrc, Vector &vecDirToEnemy)
+{
+	FireBullets( 1, vecSrc, vecDirToEnemy, TURRET_SPREAD, TURRET_RANGE, BULLET_MONSTER_9MM, 1 );
+
+	switch(RANDOM_LONG(0,2))
+	{
+	case 0: EMIT_SOUND(ENT(pev), CHAN_WEAPON, "weapons/hks1.wav", 1, ATTN_NORM); break;
+	case 1: EMIT_SOUND(ENT(pev), CHAN_WEAPON, "weapons/hks2.wav", 1, ATTN_NORM); break;
+	case 2: EMIT_SOUND(ENT(pev), CHAN_WEAPON, "weapons/hks3.wav", 1, ATTN_NORM); break;
+	}
+	pev->effects = pev->effects | EF_MUZZLEFLASH;
+}
