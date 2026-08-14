1851c611c88e809cb69d068e80e000b626ea16f7
Gastón Urgorri <urgorrigaston@gmail.com>
2026-08-13 23:50:18 -0300

    Refactor: Split dlls/triggers.cpp into separate files inside dlls/systems/
        - Split monolithic triggers.cpp into 24 distinct files in dlls/systems/.
        - Extracted CBaseTrigger into trigger_base.h and trigger_base.cpp.
        - Extracted CTriggerMultiple into trigger_multiple.h and trigger_multiple.cpp.
        - Cleaned up preambles and ensured only required defines/externs are included per file.
        - Updated dlls/Makefile to reflect the new paths and objects.
        - Updated projects/vs2019/hldll.vcxproj and filters with new trigger files.

---
+++ dlls/systems/trigger_cdaudio.cpp
@@ -0,0 +1,110 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+/*
+
+===== trigger_cdaudio.cpp ========================================================
+
+  spawn and use functions for editor-placed triggers
+
+*/
+
+#include "core/extdll.h"
+#include "core/util.h"
+#include "core/cbase.h"
+#include "player.h"
+#include "core/saverestore.h"
+#include "trains.h"			// trigger_camera has train functionality
+#include "gamerules.h"
+#include "trigger_base.h"
+
+
+class CTriggerCDAudio : public CBaseTrigger
+{
+public:
+	void Spawn( void );
+
+	virtual void Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
+	void PlayTrack( void );
+	void Touch ( CBaseEntity *pOther );
+};
+
+LINK_ENTITY_TO_CLASS( trigger_cdaudio, CTriggerCDAudio );
+
+//
+// Changes tracks or stops CD when player touches
+//
+// !!!HACK - overloaded HEALTH to avoid adding new field
+void CTriggerCDAudio :: Touch ( CBaseEntity *pOther )
+{
+	if ( !pOther->IsPlayer() )
+	{// only clients may trigger these events
+		return;
+	}
+
+	PlayTrack();
+}
+
+void CTriggerCDAudio :: Spawn( void )
+{
+	InitTrigger();
+}
+
+void CTriggerCDAudio::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
+{
+	PlayTrack();
+}
+
+void PlayCDTrack( int iTrack )
+{
+	edict_t *pClient;
+
+	// manually find the single player.
+	pClient = g_engfuncs.pfnPEntityOfEntIndex( 1 );
+
+	// Can't play if the client is not connected!
+	if ( !pClient )
+		return;
+
+	if ( iTrack < -1 || iTrack > 30 )
+	{
+		ALERT ( at_console, "TriggerCDAudio - Track %d out of range\n" );
+		return;
+	}
+
+	if ( iTrack == -1 )
+	{
+		CLIENT_COMMAND ( pClient, "cd stop\n");
+	}
+	else
+	{
+		char string [ 64 ];
+
+		sprintf( string, "cd play %3d\n", iTrack );
+		CLIENT_COMMAND ( pClient, string);
+	}
+}
+
+
+// only plays for ONE client, so only use in single play!
+void CTriggerCDAudio :: PlayTrack( void )
+{
+	PlayCDTrack( (int)pev->health );
+
+	SetTouch( NULL );
+	UTIL_Remove( this );
+}
+
+
+// This plays a CD track when fired or when the player enters it's radius
---
+++ dlls/systems/trigger_fireanddie.cpp
@@ -0,0 +1,60 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+/*
+
+===== trigger_fireanddie.cpp ========================================================
+
+  spawn and use functions for editor-placed triggers
+
+*/
+
+#include "core/extdll.h"
+#include "core/util.h"
+#include "core/cbase.h"
+#include "player.h"
+#include "core/saverestore.h"
+#include "trains.h"			// trigger_camera has train functionality
+#include "gamerules.h"
+
+
+class CFireAndDie : public CBaseDelay
+{
+public:
+	void Spawn( void );
+	void Precache( void );
+	void Think( void );
+	int ObjectCaps( void ) { return CBaseDelay::ObjectCaps() | FCAP_FORCE_TRANSITION; }	// Always go across transitions
+};
+LINK_ENTITY_TO_CLASS( fireanddie, CFireAndDie );
+
+void CFireAndDie::Spawn( void )
+{
+	pev->classname = MAKE_STRING("fireanddie");
+	// Don't call Precache() - it should be called on restore
+}
+
+
+void CFireAndDie::Precache( void )
+{
+	// This gets called on restore
+	pev->nextthink = gpGlobals->time + m_flDelay;
+}
+
+
+void CFireAndDie::Think( void )
+{
+	SUB_UseTargets( this, USE_TOGGLE, 0 );
+	UTIL_Remove( this );
+}
