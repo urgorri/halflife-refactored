74893eab1b0fb25bab1c9321c8b2f6e2168241d6
Gastón Urgorri <urgorrigaston@gmail.com>
2026-08-15 12:10:30 -0300

    fix: resolve client and server build errors with relocated AI files

--- cl_dll/hl/hl_baseentity.cpp
+++ cl_dll/hl/hl_baseentity.cpp
@@ -27,7 +27,7 @@ This file contains "stubs" of class member implementations so that we can predic
 #include "player.h"
 #include "weapons.h"
 #include "ai/nodes.h"
-#include "soundent.h"
+#include "ai/soundent.h"
 #include "skill.h"
 
 // Globals used by game logic
