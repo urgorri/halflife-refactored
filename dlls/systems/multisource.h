32838c98bae383205cbcfc1e4e03c0db60268667
Gastón Urgorri <urgorrigaston@gmail.com>
2026-08-31 01:04:50 -0300

    fix(systems): restore CMultiSource class definition in multisource.h

--- dlls/systems/multisource.h
+++ dlls/systems/multisource.h
@@ -17,6 +17,28 @@
 
 #include "core/cbase.h"
 
-// Note: CMultiSource class is also declared in cbase.h for engine visibility.
+#define MAX_MULTI_TARGETS 16 // maximum number of targets a single multi_manager entity may be assigned.
+#define MS_MAX_TARGETS 32
+
+class CMultiSource : public CPointEntity
+{
+  public:
+	void Spawn();
+	void KeyValue( KeyValueData *pkvd );
+	void Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
+	int ObjectCaps( void ) { return ( CPointEntity::ObjectCaps() | FCAP_MASTER ); }
+	BOOL IsTriggered( CBaseEntity *pActivator );
+	void EXPORT Register( void );
+	virtual int Save( CSave &save );
+	virtual int Restore( CRestore &restore );
+
+	static TYPEDESCRIPTION m_SaveData[];
+
+	EHANDLE m_rgEntities[MS_MAX_TARGETS];
+	int m_rgTriggered[MS_MAX_TARGETS];
+
+	int m_iTotal;
+	string_t m_globalstate;
+};
 
 #endif // MULTISOURCE_H
